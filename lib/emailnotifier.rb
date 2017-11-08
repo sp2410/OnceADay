895a9417fd73eab4badb999630a0de00784a8d51
saprakas@syr.edu <saprakas@syr.edu>
2017-11-08 01:07:12 -0800

    fixing styles and adding my name

--- lib/emailnotifier.rb
+++ lib/emailnotifier.rb
@@ -8,36 +8,22 @@ class EmailNotifier
 	attr_accessor :from, :to, :subject, :content
 
 	def initialize(from, to, subject, content)
-		@from = from
-		@to = to
-		@subject = subject
-		@content = content		
+		@from, @to, @subject, @content = from, to, subject, content		 
 	end
 
 
 	def personalize		
-		users = to
-
-		email = Mail.new
-		email.from = Email.new(email: @from)
-		email.subject = subject
-
+		users, email = to, Mail.new		
+		email.from, email.subject = Email.new(email: @from), subject		 
 		personalization = Personalization.new	
 		personalization.add_to(Email.new(email: "sprakash24oct@gmail.com"))
-
 		users.each do |user|																
   			personalization.add_bcc(Email.new(email: user))					
 		end
-
 		email.add_personalization(personalization)
 		email.add_content(Content.new(type: 'text/plain', value: "A new action was taken !!"))
-		email.add_content( Content.new(type: 'text/html', value: content))
-		
-
-		
-		# @email.template_id = '13b8f94f-bcae-4ec6-b752-70d6cb59f932'			
+		email.add_content( Content.new(type: 'text/html', value: content))			
 		email.reply_to = Email.new(email: 'sprakash24oct@gmail.com')
-
 		p email.to_json
 		return email
 	end
@@ -53,7 +39,6 @@ class EmailNotifier
 	def use_sendgrid_to_send_email
 		begin	
 				tosend = personalize
-
 				sg = SendGrid::API.new(api_key: ENV['SENDGRID_API_KEY'])			
 				response = sg.client.mail._('send').post(request_body: tosend.to_json)
 				puts response.status_code
--- lib/tasks/send_emails.rake
+++ lib/tasks/send_emails.rake
@@ -6,33 +6,22 @@ include ApplicationHelper
 
 task :send_email => :environment do
 	
-	posts = Post.get_all_post_ids
-	mailed_posts = MailedPost.get_all_mailedpost_ids
+	posts, mailed_posts = Post.get_all_post_ids, MailedPost.get_all_mailedpost_ids	 
 
 	posts_ids_to_randomize = (posts - mailed_posts) | (mailed_posts - posts)
 	p posts_ids_to_randomize
 
 	if posts_ids_to_randomize.size > 0	
-
-		users = User.emails_of_all_intrested_users	
-
-		post_id = posts_ids_to_randomize.sample
+		users, post_id = User.emails_of_all_intrested_users, posts_ids_to_randomize.sample
 		post = Post.find_by_id(post_id)
-
 		begin			
 			record = MailedPost.find_or_create_by(post_id: post_id)
 	        send_email(post, "Go on..Just putting this cool post: #{post.title} in your inbox. ", "Hi! When you are free have a look at : #{post.title}", users)
 	    rescue
 	    	p "email not sent "
 	    end
-
 		puts post_id
-
 	end
-
-
-
-	
 end
 
 
